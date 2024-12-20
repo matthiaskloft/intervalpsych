cc2b7de90c796cd49a88c8b1cce727bce36abad3
Matze Kloft <83857345+matthiaskloft@users.noreply.github.com>
2024-12-20 20:17:58 +0100

    added DDRM stan model

--- src/stanExports_ddrm.cc
+++ src/stanExports_ddrm.cc
@@ -2,12 +2,12 @@
 
 #include <Rcpp.h>
 using namespace Rcpp ;
-#include "stanExports_itm.h"
+#include "stanExports_ddrm.h"
 
-RCPP_MODULE(stan_fit4itm_mod) {
+RCPP_MODULE(stan_fit4ddrm_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_itm")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_ddrm")
 
     .constructor<SEXP,SEXP,SEXP>()
 
