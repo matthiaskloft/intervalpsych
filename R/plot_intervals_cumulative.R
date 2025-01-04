4c8af6c014ddd6fa1be85c85b43059c8121732a5
Matthias <mkaystrat88@gmail.com>
2025-01-04 14:17:19 +0100

    update: interval plot cumulative

--- R/plot_intervals_cumulative.R
+++ R/plot_intervals_cumulative.R
@@ -1,138 +1,188 @@
+#' Gather Values from Intervals
+#'
+#' This function gathers values from specified intervals, either weighted by interval width or not.
+#'
+#' @param lower A numeric vector of lower bounds of the intervals.
+#' @param upper A numeric vector of upper bounds of the intervals.
+#' @param cluster_id An optional vector of cluster IDs. If not provided, a vector of ones will be created.
+#' @param weighted A logical value indicating whether the values should be weighted by interval width.
+#' @param step_size A numeric value specifying the step size when `weighted` is FALSE.
+#' @param n_samples A numeric value specifying the number of samples when `weighted` is TRUE.
+#'
+#' @return A data frame with columns `samples` and `cluster_id`.
+#'
+#' @details
+#' - If `weighted` is TRUE, values are sampled uniformly within each interval.
+#' - If `weighted` is FALSE, values are gathered at regular intervals specified by `step_size`.
+#'
+#' @importFrom purrr map_dfr
+#' @importFrom stats runif
+#'
+gather_values <- function(lower,
+                          upper,
+                          cluster_id = NULL,
+                          weighted = NULL,
+                          step_size = NULL,
+                          n_samples = NULL) {
+  # check that lower and upper are the same length
+  if (length(lower) != length(upper)) {
+    stop("lower and upper must be the same length")
+  }
 
+  # check that lower and upper are numeric
+  if (!is.numeric(lower) | !is.numeric(upper)) {
+    stop("lower and upper must be numeric")
+  }
 
-plot_intervals_cumulative <- function(lower,
-                                      upper,
-                                      cluster_id,
-                                      cluster_label = NA,
-                                      truth = NA,
-                                      min,
-                                      max,
-                                      facet_wrap = FALSE,
-                                      design = NULL,
-                                      step_size,
-                                      show_quantiles = TRUE,
-                                      ncol = 2) {
+  # check tha lower is smaller than upper or to equal
+  if (any(lower > upper)) {
+    stop("lower must NOT be greater than upper")
+  }
 
-  # gather values between bounds
-  gathered <-
-    gather_values(lower, upper, cluster_id, step_size = step_size) %>%
-    group_by(cluster_id) %>%
-    # compute the maximum density
-    mutate(
-      max_density = max(table(round(
-        samples, -floor(log10(step_size))
-      )), na.rm = TRUE) %>%
-        as.double(),
-      median = median(samples, na.rm = TRUE),
-      q_05 = quantile(samples, probs = .05),
-      q_95 = quantile(samples, probs = .95)
-    ) %>%
-    ungroup() %>%
-    full_join(
-      data.frame(
-        lower_mean = lower_mean,
-        upper_mean = upper_mean,
-        lower_mean_logit = lower_mean_logit,
-        upper_mean_logit = upper_mean_logit,
-        truth = as.numeric(truth),
-        cluster_id = cluster_id,
-        item_name = item_name
-      ) %>%
-        distinct()
-    ) %>%
-    mutate(cluster_id = factor(cluster_id),
-           item_name = factor(item_name))
+  # check that weighted is not NULL
+  if (is.null(weighted)) {
+    stop("weighted must be specified")
+  }
 
-  plot <-
-    try(ggplot_aggregated_single(
-      data = gathered,
-      binwidth = step_size,
-      min = min,
-      max = max,
-      facet_wrap = facet_wrap,
-      design = design,
-      show_quantiles = show_quantiles,
-      ncol = ncol
-    ))
-  return(plot)
+  # if cluster_id is NOT NULL, check that it is the same length as lower and upper
+  if (!is.null(cluster_id)) {
+    if (length(cluster_id) != length(lower)) {
+      stop("cluster_id must be the same length as lower and upper")
+    }
+  }
+
+  # if cluster_id is NULL, create a vector of ones
+  if (is.null(cluster_id)) {
+    cluster_id <- rep(1, length(lower))
+  }
+
+  # weighted = TRUE -----------------------------------------------------------
+  # values are weighted by interval width
+  if (weighted) {
+    # check that n_samples is not NULL
+    if (is.null(n_samples)) {
+      stop("n_samples must be specified when weighted = TRUE")
+    }
+
+    # map over intervals
+    df_out <- purrr::map_dfr(1:length(lower), function(.x) {
+      # if lower is equal to upper, return lower
+      if (lower[.x] == upper[.x]) {
+        samples <- rep(lower[.x], n_samples)
+
+      } else {
+        samples <- stats::runif(n = n_samples,
+                                min = lower[.x],
+                                max = upper[.x])
+      }
+
+      id <- rep(cluster_id[.x], length(samples))
+
+      return(data.frame(samples = samples, cluster_id = id))
+    })
+  }
+
+  # weighted = FALSE ---------------------------------------------------------
+  # values are NOT weighted by interval width
+  else {
+    # check that step_size is not NULL
+    if (is.null(step_size)) {
+      stop("step_size must be specified when weighted = FALSE")
+    }
+
+    # map over intervals
+    df_out <- purrr::map_dfr(1:length(lower), function(.x) {
+      # gather values between bounds by step size
+      samples <- seq(lower[.x], upper[.x], by = step_size) |> as.double()
+      id <- rep(cluster_id[.x], length(samples))
+
+      return(data.frame(samples = samples, cluster_id = id))
+    })
+
+  }
+
+  return(df_out)
 }
 
-# helper: gather interval values -----------------------------------------------
-gather_values <- function(lower, upper, cluster_id, step_size) {
-  # identify columns for lower and upper response value
 
-
-  df_out <- map_dfr(1:length(lower), function(.x) {
-    # gather values between bounds by step size
-    samples <- seq(lower[.x], upper[.x], by = step_size) %>% as.double()
-    cluster_id <- rep(cluster_id[.x], length(samples))
-
-    return(data.frame(samples = samples, cluster_id = cluster_id))
-  })
-
-  return(df_out %>% arrange(cluster_id))
-}
-
-# helper: ggplot for aggregated responses --------------------------------------
-ggplot_aggregated_single <-
+#' @title Plot Cumulative Intervals
+#' @description This function creates a cumulative interval plot using ggplot2.
+#' @param data A data frame containing the data to be plotted.
+#' @param min The minimum value for the x-axis.
+#' @param max The maximum value for the x-axis.
+#' @param binwidth The width of the bins for the histogram.
+#' @param facet_wrap A logical value indicating whether to use facet wrapping.
+#' @param design An optional design matrix for facet wrapping.
+#' @param show_quantiles A logical value indicating whether to show quantiles on the plot.
+#' @param ncol The number of columns for facet wrapping if design matrix is not provided.
+#' @return A ggplot2 object representing the cumulative interval plot.
+#'
+ggplot_cumulative_intervals <-
   function(data,
-           min = min,
-           max = max,
-           binwidth,
+           min = NULL,
+           max = NULL,
+           binwidth = NULL,
            facet_wrap = FALSE,
-           design = NULL,
            show_quantiles = TRUE,
            ncol = 2) {
-    # avoid warnings
-    #samples <- NULL
+    # check that min and max are not NULL
+    if (is.null(min) | is.null(max)) {
+      stop("min and max must be specified")
+    }
 
-    # plot
+    # check that binwidth is not NULL
+    if (is.null(binwidth)) {
+      stop("binwidth must be specified")
+    }
+
+    # plot ---------------------------------------------------------------------
     plot <-
-      ggplot2::ggplot(data, ggplot2::aes(x = samples)) +
-
-      ggplot2::geom_area(
-        stat = "bin",
-        color = "black",
-        fill = "gray95",
-        binwidth = binwidth,
-        linewidth = .7
-      ) +
-      ggplot2::geom_vline(
-        aes(xintercept = truth),
-        color = ggokabeito::palette_okabe_ito(order = 1),
-        line_width = 1
-      ) +
-      ggplot2::geom_errorbarh(
-        ggplot2::aes(
-          xmin = lower_mean_logit,
-          xmax = upper_mean_logit,
-          y = max_density * 1.15,
-          height = max_density * .05
-        ),
-        col = "grey70",
-        height = 0,
-        linetype = 1,
-        linewidth = 8
-      ) +
-      ggplot2::geom_errorbarh(
-        ggplot2::aes(
-          xmin = lower_mean,
-          xmax = upper_mean,
-          y = max_density * 1.15,
-          height = max_density * .1
-        ),
-        col = "black",
-        linetype = 1,
-        linewidth = 1.5
-      ) +
-      ggplot2::scale_x_continuous(
-        limits = c(min, max),
+      ggplot2::ggplot(data, ggplot2::aes(x = .data$samples)) +
+      ggdist::stat_slab(aes(.data$samples), density = "bounded", fill = "gray95", color = "black", alpha = 0.5) +
+      # ggplot2::geom_area(
+      #   stat = "bin",
+      #   color = "black",
+      #   fill = "gray95",
+      #   binwidth = binwidth,
+      #   linewidth = .7
+      # ) +
+      # ggplot2::geom_vline(
+      #   aes(xintercept = truth),
+      #   color = ggokabeito::palette_okabe_ito(order = 1),
+      #   linewidth = 1
+      # ) +
+      # ggplot2::geom_errorbarh(
+      #   ggplot2::aes(
+      #     xmin = lower_mean_logit,
+      #     xmax = upper_mean_logit,
+      #     y = max_density * 1.15,
+      #     height = max_density * .05
+      #   ),
+      #   col = "grey70",
+      #   height = 0,
+      #   linetype = 1,
+      #   linewidth = 8
+      # ) +
+      # ggplot2::geom_errorbarh(
+      #   ggplot2::aes(
+      #     xmin = lower_mean,
+      #     xmax = upper_mean,
+      #     y = max_density * 1.15,
+      #     height = max_density * .1
+      #   ),
+      #   col = "black",
+      #   linetype = 1,
+      #   linewidth = 1.5
+      # ) +
+      ggplot2::scale_x_continuous(limits = c(min(.data$min, na.rm = TRUE), max(.data$max, na.rm = TRUE)),
+                                  #labels = c(c("0", ".25", ".50", ".75", "1")),
+                                  #breaks = seq(min, max, length.out = 5),
+                                  expand = ggplot2::expansion()) +
+      ggplot2::scale_y_continuous(
         labels = c(c("0", ".25", ".50", ".75", "1")),
-        breaks = seq(min, max, length.out = 5),
-        expand = ggplot2::expansion()
-      ) +
-      ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0, .05))) +
-      ggplot2::labs(x = "Response Value", y = "Cumulative Pointwise Frequency") +
+        breaks = seq(0, 1, .25),
+        expand = ggplot2::expansion(mult = c(0, .01))) +
+      ggplot2::labs(x = "Response Value", y = "Density") +
       theme_itm() +
       theme(
         plot.margin = margin(.2, .5, .2, .2, "cm"),
@@ -140,43 +190,120 @@ ggplot_aggregated_single <-
         axis.line = element_line(colour = "#6d6d6e", size = .3),
         axis.ticks = element_line(colour = "#6d6d6e", size = .3),
         axis.text = element_text(size = 10),
-        strip.text = element_text(margin = margin(0, 0, .3, 0, "cm")),
-      ) +
+        strip.text = element_text(size = 11, margin = margin(0, 0, .3, 0, "cm"))
+      )
 
-      ggplot2::theme(#strip.text = ggplot2::element_text(size = 14),
-        #strip.background = ggplot2::element_rect(fill = "gray95"),
-        #strip.clip = "on")
-        # add facet wrap
-        if (facet_wrap == TRUE) {
-          if (is.null(design)) {
-            plot <- plot + ggplot2::facet_wrap( ~ item_name, scales = "free", ncol = ncol)
-          } else {
-            plot <- plot +
-              facet_manual( ~ item_name, design = design, scales = "free")
-          }
-        }
+    # add facet wrap ---------------------------------------------------------
 
-        if (show_quantiles == TRUE) {
-          # add quantiles to the plot
-          plot <- plot +
-            ggplot2::geom_vline(
-              ggplot2::aes(xintercept = median),
-              col = "black",
-              linetype = 1,
-              linewidth = .6
-            ) +
-            ggplot2::geom_vline(
-              ggplot2::aes(xintercept = q_05),
-              col = "black",
-              linetype = 2,
-              linewidth = .7
-            ) +
-            ggplot2::geom_vline(
-              ggplot2::aes(xintercept = q_95),
-              col = "black",
-              linetype = 2,
-              linewidth = .7
-            )
-        }
-        return(plot)
+    if (facet_wrap) {
+        plot <- plot + ggplot2::facet_wrap( ~ cluster_id, scales = "free", ncol = ncol)
+    }
+
+    # add quantiles to the plot ------------------------------------------------
+    if (show_quantiles) {
+      plot <- plot +
+        ggplot2::geom_vline(
+          ggplot2::aes(xintercept = .data$median),
+          col = "black",
+          linetype = 1,
+          linewidth = .6
+        ) +
+        ggplot2::geom_vline(
+          ggplot2::aes(xintercept = .data$q_05),
+          col = "black",
+          linetype = 2,
+          linewidth = .7
+        ) +
+        ggplot2::geom_vline(
+          ggplot2::aes(xintercept = .data$q_95),
+          col = "black",
+          linetype = 2,
+          linewidth = .7
+        )
+    }
+
+    return(plot)
   }
+
+
+#' Plot Cumulative Intervals
+#'
+#' This function generates a cumulative interval plot based on the provided lower and upper bounds, cluster IDs, and other optional parameters.
+#'
+#' @param lower A numeric vector of lower bounds.
+#' @param upper A numeric vector of upper bounds.
+#' @param cluster_id A vector of cluster IDs corresponding to the intervals.
+#' @param truth A numeric vector of ground truth values. Default is NA.
+#' @param min The minimum value for the x-axis.
+#' @param max The maximum value for the x-axis.
+#' @param facet_wrap A logical value indicating whether to use facet wrapping. Default is FALSE.
+#' @param design An optional design parameter for the plot.
+#' @param weighted An optional vector of weights for the intervals.
+#' @param show_quantiles A logical value indicating whether to show quantiles on the plot. Default is TRUE.
+#' @param ncol The number of columns for facet wrapping. Default is 2.
+#'
+#' @return A ggplot object representing the cumulative interval plot.
+#'
+#' @importFrom stats median
+#' @importFrom stats quantile
+#'
+#' @export
+#'
+plot_intervals_cumulative <- function(lower,
+                                      upper,
+                                      cluster_id,
+                                      truth = NA,
+                                      min,
+                                      max,
+                                      facet_wrap = FALSE,
+                                      weighted = NULL,
+                                      show_quantiles = TRUE,
+                                      ncol = 2) {
+
+  step_size <- min(max - min) / 1e3
+  n_samples <- 1e3
+
+  # gather values between bounds
+  df_samples <-
+    gather_values(
+      lower = lower,
+      upper = upper,
+      cluster_id = cluster_id,
+      weighted = weighted,
+      step_size = step_size,
+      n_samples = n_samples
+    ) |>
+    dplyr::group_by(cluster_id) |>
+    # compute the maximum density
+    dplyr::mutate(
+      max_density = max(table(round(
+        samples, -floor(log10(step_size))
+      )), na.rm = TRUE) |>
+        as.double(),
+      median = median(samples, na.rm = TRUE),
+      q_05 = quantile(samples, probs = .05),
+      q_95 = quantile(samples, probs = .95)
+    ) |>
+    dplyr::ungroup()
+
+  # join samples with ground truth
+  df_plot <-
+    dplyr::full_join(df_samples,
+              data.frame(truth = as.numeric(truth), cluster_id = cluster_id) |>
+                dplyr::distinct()) |>
+    dplyr::mutate(cluster_id = factor(cluster_id))
+
+  # plot
+  plot <-
+    ggplot_cumulative_intervals(
+      data = df_plot,
+      binwidth = step_size,
+      min = min,
+      max = max,
+      facet_wrap = facet_wrap,
+      show_quantiles = show_quantiles,
+      ncol = ncol
+    )
+
+  return(plot)
+}
